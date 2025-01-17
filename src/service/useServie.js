3b6ee3aa71df54b5ef41666b4653822e8d983a3e
Anyaele Gabriel <99973182+Chimex2022@users.noreply.github.com>
2025-01-17 21:38:20 +0100

    Update useServie.js

--- src/service/useServie.js
+++ src/service/useServie.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "http://localhost:8080/api/users"; 
+const API_URL = "https://mern-user-back.onrender.com/api/users"; 
 
 export const getUsers = async () => {
   const response = await axios.get(API_URL);
